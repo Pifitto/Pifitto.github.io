5dcfe80f90c5f6b392be2c618121c2e36d8bdcef
Pfitto <82768046+Pfitto@users.noreply.github.com>
2022-01-22 17:30:47 +0100

    cambi
    invio

---
+++ main.js
@@ -0,0 +1,9 @@
+function toggle(){
+	var menu = document.getElementById('menu-content');
+	var b1 = document.getElementById('menu');
+	var coperto = document.getElementById('coperto');
+	coperto.classList.toggle('active')
+	b1.classList.toggle('active')
+	menu.classList.toggle('active')
+}
+
