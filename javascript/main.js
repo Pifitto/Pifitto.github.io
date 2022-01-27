ceaf16490cb90683580d13a27016997f04d475fe
Pfitto <82768046+Pfitto@users.noreply.github.com>
2022-01-27 19:25:00 +0100

    si

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,17 @@
+window.onscroll = function() {
+  stickNav()
+};
+
+var nav = document.getElementById("title");
+var sticky = nav.offsetTop;
+
+function stickNav() {
+  if (window.pageYOffset > sticky) {
+    nav.classList.toggle('sticky');
+  } else {
+    nav.classList.remove("sticky");
+  }
+}
 function toggle(){
 	var menu = document.getElementById('menu-content');
 	var b1 = document.getElementById('menu');
